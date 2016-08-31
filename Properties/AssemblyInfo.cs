d350261dd72ca15b5614250e82a281fdcdfc0aa5
pnavarra <pnavarra@coria.com>
2016-08-31 01:04:44 -0400

    Update for 8.2 Initial Release

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,16 +1,15 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Sitecore.SharedSource.ValueListField")]
-[assembly: AssemblyDescription("Sitecore Module")]
+[assembly: AssemblyDescription("Sitecore Marketplace Module")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Pete Navarra")]
+[assembly: AssemblyCompany("DragonByte LLC - Pete Navarra")]
 [assembly: AssemblyProduct("Sitecore.SharedSource.ValueListField")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2015")]
+[assembly: AssemblyCopyright("Copyright © DragonBye LLC 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyVersion("1.2.1.0")]
+[assembly: AssemblyFileVersion("1.2.1.0")]
